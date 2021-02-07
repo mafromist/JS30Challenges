44b129b430ed187e6fbc562721f55b06dad43364
MugeMeggy <mafromist@gmail.com>
2021-02-07 22:55:26 +0000

    #JavaScript30 #2 has finished

--- Challenge 1 - JS Drum Kit/app.js
+++ Challenge 1 - JS Drum Kit/app.js
@@ -1,20 +1,18 @@
 const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
-    if(!audio) {
-        return
-    }
+    if(!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
 }
 
-const removeTransition = (e) => {
-   if (e.propertyName !== 'transform') return;
-   this.classList.remove("playing"); // REMOVE LOOKS LIKE INDEFINED BY THE BROWSER
+let removeTransition = (e) => {
+    if (e.propertyName !== 'transform') return;
+   e.target.classList.remove('playing'); 
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = document.querySelectorAll(`.key`);
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
